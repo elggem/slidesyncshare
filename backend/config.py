49c84643659badad4872ccd367212d187c6b9ea1
Ralf Mayet <trilly@gmail.com>
2021-01-12 18:23:44 +0100

    fix certificate that is not needed

--- backend/config.py
+++ backend/config.py
@@ -9,15 +9,10 @@ WTF_CSRF_ENABLED = True
 # secret key for authentication
 SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "you-will-never-guess")
 
-CONSUMER_KEY_PEM_FILE = os.path.abspath('consumer_key.pem')
-with open(CONSUMER_KEY_PEM_FILE, 'w') as wfile:
-    wfile.write(os.environ.get('CONSUMER_KEY_CERT', ''))
-
 PYLTI_CONFIG = {
     "consumers": {
         "__consumer_key__": {
-            "secret": os.environ.get("CONSUMER_KEY_SECRET", "__lti_secret__"),
-            "cert": CONSUMER_KEY_PEM_FILE
+            "secret": os.environ.get("CONSUMER_KEY_SECRET", "__lti_secret__")
         }
     }
 }
